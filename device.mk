ce0159f300e4cd62c5b417968bc2175b5704a9ae
Steve Kondik <shade@chemlab.org>
2013-05-17 13:10:19 -0700

    onyx: Increase heap start size to 16m to minimize GC with larger bitmaps
    Change-Id: I89af5e94c9aafdc6007c84d3fce60e4db349e967

--- device.mk
+++ device.mk
@@ -270,7 +270,7 @@ PRODUCT_PACKAGES += \
 
 # Dalvik heap
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
