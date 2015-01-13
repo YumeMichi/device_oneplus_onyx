c27beb36bd6bc86a1f0ed940a26c88fbf6f0d610
Ethan Chen <intervigil@gmail.com>
2015-01-12 19:41:34 -0800

    onyx: Add assertions on TrustZone version
    Change-Id: Ice534e51366834dee855a59832277421e7ffb274

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,10 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
+# Releasetools
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_onyx
+TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 
