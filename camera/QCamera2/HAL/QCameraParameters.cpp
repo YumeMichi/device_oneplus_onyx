85afb2f83eeaa8e0bc435864ea11267bda52a150
Sultanxda <sultanxda@gmail.com>
2015-07-13 13:12:27 -0700

    QCamera2: Disable JPEG compression
    Change-Id: I7f8d30eed8505132674392a1ae1cafdb0ea69805

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -7660,11 +7660,7 @@ int QCameraParameters::getBurstNum()
  *==========================================================================*/
 int QCameraParameters::getJpegQuality()
 {
-    int quality = getInt(KEY_JPEG_QUALITY);
-    if (quality < 0) {
-        quality = 98; // set to default quality value
-    }
-    return quality;
+    return 100;
 }
 
 
