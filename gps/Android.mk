89746996b3da64f1b6e535dbf1640feaa55118bf
Ashwin Ramesh <ashwinr64@gmail.com>
2016-01-12 06:01:32 +0530

    onyx: Fix the build-barrier for GPS HAL

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,9 @@
 # limitations under the License.
 #
 
-ifneq ($(filter bacon,$(TARGET_DEVICE)),)
+ifneq ($(filter onyx,$(TARGET_DEVICE)),)
 
 LOC_PATH := $(call my-dir)
-
 include $(call first-makefiles-under,$(LOC_PATH))
 
 endif
