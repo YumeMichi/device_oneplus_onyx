28742fb4456065ab011b496a67f92ec1781d0145
Ashwin Ramesh <ashwinr64@gmail.com>
2016-01-17 12:20:01 +0530

    onyx: Kill redundant mkbootimg.mk
    Change-Id: I2ef966bbd773f56f81e0537fd50b5ae4e789983f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=onyx user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-# BOARD_CUSTOM_BOOTIMG_MK := $(PLATFORM_PATH)/mkbootimg.mk
 BOARD_KERNEL_PREBUILT_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/oneplus/onyx/dt.img
 TARGET_KERNEL_ARCH := arm
