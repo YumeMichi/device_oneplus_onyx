61a235a70a43953e9f19dacfa65b669f36fadc33
Steve Kondik <steve@cyngn.com>
2016-01-22 05:59:33 -0800

    onyx: Copy power config to recovery
    Change-Id: Ibc7352db2148fd4dd3b37906106a6e36c8883e41

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -74,7 +74,7 @@ LOCAL_MODULE       := init.recovery.onyx.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.onyx.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
