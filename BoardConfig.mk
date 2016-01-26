458a8d2c2221461157b905b15d830fcb27aa026a
Ashwin <ashwinr64@gmail.com>
2016-01-26 19:49:33 +0530

    onyx: Enforce SELinux
    * TODO:Write policies for tri-state keys (configpanel should be fixed as well)
    
    Change-Id: I43318e56147ec6490abdb47d7f00cd0abc5856a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ TARGET_CPU_VARIANT := krait
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=onyx user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=onyx user_debug=23 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_PREBUILT_DT := true
