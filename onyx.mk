022837d0545dc1d0f6be91c2dff45bc1022b983d
Willi Ye <williye97@gmail.com>
2016-01-28 15:40:10 +0100

    onyx: introduce dtbToolONYX
    Change-Id: I10b4f12ce6a4a2e32ef86eea1d4bbd2cc74fbdb1

--- onyx.mk
+++ onyx.mk
@@ -66,6 +66,10 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# dtbTool
+PRODUCT_PACKAGES += \
+    dtbToolONYX
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
