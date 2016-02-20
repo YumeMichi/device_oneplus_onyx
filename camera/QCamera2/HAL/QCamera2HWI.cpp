3889ea7b18c05dda367e74c305b3376d6c4d8a33
Sultanxda <sultanxda@gmail.com>
2016-02-20 12:12:44 -0800

    QCamera2: Disable thermal adapter
    We're not using userspace thermal control. This fixes the lag when
    switching between the front and rear camera.
    
    Change-Id: I9d3a2d796f10ca0300780e1e8086895d24f4816a

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1156,9 +1156,9 @@ int QCamera2HardwareInterface::openCamera(struct hw_device_t **hw_device)
     rc = openCamera();
     if (rc == NO_ERROR){
         *hw_device = &mCameraDevice.common;
-        if (m_thermalAdapter.init(this) != 0) {
-          ALOGE("Init thermal adapter failed");
-        }
+        //if (m_thermalAdapter.init(this) != 0) {
+        //  ALOGE("Init thermal adapter failed");
+        //}
     }
     else
         *hw_device = NULL;
@@ -1352,7 +1352,7 @@ int QCamera2HardwareInterface::closeCamera()
         }
     }
 
-    m_thermalAdapter.deinit();
+    //m_thermalAdapter.deinit();
 
     // delete all channels if not already deleted
     for (i = 0; i < QCAMERA_CH_TYPE_MAX; i++) {
