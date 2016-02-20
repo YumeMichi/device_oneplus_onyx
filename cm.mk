dc8d23888575721edfb5bc4d030a09e6c49e68b5
Ashwin Ramesh <ashwinr64@gmail.com>
2016-02-20 15:13:00 +0530

    onyx: cm.mk: Do some branding
    * Nope,no NFC here.
    * Set proper branding to fix misc. issues like "ok google" not recognising
    * Fix copypasta in audio_policy.
    
    Change-Id: Iab272478682e23d53bb6f3c826c7d93013d443ab

--- cm.mk
+++ cm.mk
@@ -18,9 +18,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/onyx.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -30,3 +27,7 @@ PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := OnePlus X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := onyx
