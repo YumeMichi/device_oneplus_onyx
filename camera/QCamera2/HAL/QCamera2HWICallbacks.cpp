a720889794e2ec46ee2718792fd1421605cbd8b4
Sultanxda <sultanxda@gmail.com>
2016-04-19 22:24:03 -0700

    QCamera2: Fix choppy video preview in low-light after recording video
    The recording hint must be disabled in order to fix a bug in Google Camera.
    However, the anti-shake algorithm uses the recording hint value to determine
    whether camcorder mode is enabled.
    
    Cache the userspace-set recording hint value and use that for anti-shake instead
    to ensure that the proper exposure mode setting is selected.
    
    Change-Id: I65d0a90f03e3d1214a3898672f53fe0aef055c53

--- camera/QCamera2/HAL/QCamera2HWICallbacks.cpp
+++ camera/QCamera2/HAL/QCamera2HWICallbacks.cpp
@@ -1256,7 +1256,7 @@ void QCamera2HardwareInterface::processAntishakeAlgo(QCamera2HardwareInterface *
         pme->mHfrFrameCount = 0;
     }
 
-    if (pme->mParameters.getRecordingHintValue()) {
+    if (pme->mParameters.getAppRecordingHint()) {
         if (real_gain < REAL_GAIN_DISABLE_THRESH) {
             pme->mParameters.setPrvwExpTime(EXP_TIME_OVERRIDE_DISABLE);
         } else if (real_gain > REAL_GAIN_LOW_THRESH) {
