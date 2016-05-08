01d47fb3595542b6a5e6c49ae84c23e79402ce99
Sultanxda <sultanxda@gmail.com>
2016-05-07 17:07:30 -0700

    QCamera2: Attempt to open camera again if first try fails
    Preventive measure to be safe rather than sorry
    
    Change-Id: I5dbfa283a00621793140042732c6e906a7e997f3

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1165,8 +1165,11 @@ int QCamera2HardwareInterface::openCamera()
 
     mCameraHandle = camera_open(mCameraId);
     if (!mCameraHandle) {
-        ALOGE("camera_open failed.");
-        return UNKNOWN_ERROR;
+        mCameraHandle = camera_open(mCameraId);
+        if (!mCameraHandle) {
+            ALOGE("camera_open failed.");
+            return UNKNOWN_ERROR;
+        }
     }
     if (NULL == gCamCapability[mCameraId])
         initCapabilities(mCameraId,mCameraHandle);
