2916d2e3200b57794682b56ce328ddd84cf0c3bf
Sultanxda <sultanxda@gmail.com>
2016-05-07 17:08:49 -0700

    QCamera2: Update for Marshmallow compilation
    Change-Id: I5e1b43d1d9b46158d3bc56fec48645bf4bc32851

--- camera/QCamera2/HAL/QCamera2Hal.cpp
+++ camera/QCamera2/HAL/QCamera2Hal.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundataion. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -48,5 +48,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
     set_callbacks: NULL,
     get_vendor_tag_ops: NULL,
     open_legacy: NULL,
+    set_torch_mode: NULL,
+    init: NULL,
     reserved:  {0}
 };
