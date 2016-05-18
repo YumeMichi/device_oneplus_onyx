b02c1e2dac347f42370fcf829c44569f80a7cfad
Steve Kondik <steve@cyngn.com>
2016-05-18 12:54:14 -0700

    onyx: Reenable the QC location stack
     * Mystery solved, it works.
    
    This reverts commit ae50abf602ea5ede5908603111ec80ee4f3bcdf8.
    
    Change-Id: I6c33e07c517702331f05650dc85c84ded9781ec1

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -57,6 +57,7 @@ done
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \\
+    com.qualcomm.location \\
     qcrilmsgtunnel \\
     shutdownlistener \\
     libril
@@ -108,6 +109,17 @@ EOF
 LOCAL_PATH := \$(call my-dir)
 
 ifneq (\$(filter onyx,\$(TARGET_DEVICE)),)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
