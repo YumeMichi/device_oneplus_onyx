5831f7c775bb14cae3214abd3b7341dd278c4b25
Sultanxda <sultanxda@gmail.com>
2016-06-10 16:55:46 -0700

    QCamera2: Remove auto-focus zone options for front camera
    Front camera does not have an actuator.
    
    Change-Id: Ie9ffc3180f985f9febd338cab949ec81f5725a50

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -4439,13 +4439,15 @@ int32_t QCameraParameters::initDefaultParameters()
     setHighFrameRate(CAM_HFR_MODE_OFF);
 
     // Set Focus algorithms
-    String8 focusAlgoValues = createValuesString(
-            (int *)m_pCapability->supported_focus_algos,
-            m_pCapability->supported_focus_algos_cnt,
-            FOCUS_ALGO_MAP,
-            sizeof(FOCUS_ALGO_MAP) / sizeof(QCameraMap));
-    set(KEY_QC_SUPPORTED_FOCUS_ALGOS, focusAlgoValues);
-    setSelectableZoneAf(FOCUS_ALGO_AUTO);
+    if (m_pCapability->position == CAM_POSITION_BACK) {
+        String8 focusAlgoValues = createValuesString(
+                (int *)m_pCapability->supported_focus_algos,
+                m_pCapability->supported_focus_algos_cnt,
+                FOCUS_ALGO_MAP,
+                sizeof(FOCUS_ALGO_MAP) / sizeof(QCameraMap));
+        set(KEY_QC_SUPPORTED_FOCUS_ALGOS, focusAlgoValues);
+        setSelectableZoneAf(FOCUS_ALGO_AUTO);
+    }
 
     // Set Zoom Ratios
     if (m_pCapability->zoom_supported > 0) {
