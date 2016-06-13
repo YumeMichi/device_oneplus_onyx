4e560011e4beee8729033277327373390c89026b
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-13 21:27:43 +0300

    cmhw: Use cmsdk FileUtils
    Change-Id: Idebbc357888c8670829a70c69c2fa99f717b45c6

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -31,14 +31,15 @@ public class KeyDisabler {
 
     private static String CONTROL_PATH = "/proc/touchpanel/keypad_enable";
 
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return FileUtils.isFileWritable(CONTROL_PATH);
+    }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return FileUtils.readOneLine(CONTROL_PATH).equals("0");
     }
 
     public static boolean setActive(boolean state) {
         return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
     }
-
 }
