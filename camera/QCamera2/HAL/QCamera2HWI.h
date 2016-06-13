ca235586b1e7f4e3487fcee42d519bfc4751ccb7
Suman Mukherjee <sumam@codeaurora.org>
2016-06-13 16:23:08 +0530

    BACKPORT: QCamera2: HAL1: Close duplicated FD's for media extension.
    In video metadata mode, framework allocates native handle
    resource and needs to be freed in release recording.
    This change is to free framework resources in case of media
    extension use cases.
    
    [ashwin]: Adapt for msm8x74 HAL
    
    Change-Id: I1bac10740130d506d71ec1275dd7d75f66c263c8

--- camera/QCamera2/HAL/QCamera2HWI.h
+++ camera/QCamera2/HAL/QCamera2HWI.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2012-2014,2016 The Linux Foundataion. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -648,6 +648,7 @@ private:
     bool mAdvancedCaptureConfigured;
     bool mPreviewFrameSkipValid;
     cam_frame_idx_range_t mPreviewFrameSkipIdxRange;
+    QCameraVideoMemory *mVideoMem;
 };
 
 }; // namespace qcamera
