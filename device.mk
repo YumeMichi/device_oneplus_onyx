160be03a990f839932fbfd52ec8aecfff9adac2e
Sultanxda <sultanxda@gmail.com>
2016-06-25 11:50:11 -0700

    onyx: Fix missing torch tile for encrypted users
    The torch tile is registered and appears fine when encryption isn't used
    (thanks to several hacks inside the camera HAL), but the camera HAL hacks
    are not holding up when encryption is used. The restart-media hack is still
    needed right now to ensure the torch tile is registered successfully when
    encryption is used; however, restarting media after boot is completed on
    an unencrypted device causes the torch tile to go missing sometimes.
    
    Introduce an init script to check if encryption is present in order to
    restart media when it is needed, so that the torch tile appears all the time
    for both encrypted and unencrypted users.

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
-    init.qcom.bt.sh
+    init.qcom.bt.sh \
+    init.restart.media.sh
 
 # Permissions
 PRODUCT_COPY_FILES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -68,3 +68,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.restart.media.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.restart.media.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
