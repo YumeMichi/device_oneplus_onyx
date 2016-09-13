7b3c49bd7240897090ea47411f8422236607ef95
Ashwin Ramesh <ashwinr64@gmail.com>
2016-09-13 10:59:34 +0530

    onyx: Add missing CameraParameters
    Change-Id: Ibcb80a7cd4e68929fca86cf2b9cedc3213518af5

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -15,7 +15,11 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
-const char CameraParameters::KEY_APP_MASK[] = "app-mask";
+    const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual"; \
+    const char CameraParameters::KEY_APP_MASK[] = "app-mask"; \
+    const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct";
 
 #define CAMERA_PARAMETERS_EXTRA_H \
-    static const char KEY_APP_MASK[];
+    static const char FOCUS_MODE_MANUAL_POSITION[]; \
+    static const char KEY_APP_MASK[]; \
+    static const char WHITE_BALANCE_MANUAL_CCT[];
