0b1044aded9bd9186bea6e9cdf95aaacd54bf290
Ashwin Ramesh <ashwinr64@gmail.com>
2016-10-18 13:53:43 +0530

    onyx: libshims: Add a shim for Dynamic sensor list
    * Our HAL doesn't support Dynamic sensor list introduced in N
    
    Change-Id: I6f4294d3e0956a3f95cc654573231f5735a9bee0

---
+++ libshims/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := sensors.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libshims_sensors
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ libshims/sensors.cpp
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+
+extern "C" ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* kewl, void* list);
+
+extern "C" ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* kewl, void* list) {
+    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(kewl, list);
+    }
