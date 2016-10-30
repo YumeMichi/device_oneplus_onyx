f4fe1205a77da2c2afdd086b84a3fa8b5e57764d
Ethan Chen <intervigil@gmail.com>
2016-10-30 12:44:42 +0200

    onyx: Add GraphicBuffer shim for camera
    * Needed by Old camera HAL
    
    Change-Id: Idb1860a05b3669581951ac303bdefbb8ff0c8a99

--- device.mk
+++ device.mk
@@ -125,6 +125,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     libshims_atomic \
+    libshims_camera \
     Snap
 
 # Camera configurations
