e21842cc640aa35337c6374179f7651b5d5381ac
Suman Mukherjee <sumam@codeaurora.org>
2016-11-10 15:19:59 +0530

    QCamera2: HAL: Media extension flag enhancement
    Addded USE_MEDIA_EXTENSION flag properly across the code.
    In closenativehandle for null metadata, removed error return
    as that'll cause VT call to fail.
    
    Change-Id: Ie3bb20463a8dbf23a96f2ae288ffbfb69b81d958

--- camera/QCamera2/HAL/QCameraMem.cpp
+++ camera/QCamera2/HAL/QCameraMem.cpp
@@ -997,6 +997,7 @@ camera_memory_t *QCameraStreamMemory::getMemory(int index, bool metadata) const
     return mCameraMemory[index];
 }
 
+#ifdef USE_MEDIA_EXTENSIONS
 /*===========================================================================
 * FUNCTION   : getNativeHandle
 
@@ -1051,11 +1052,11 @@ int QCameraVideoMemory::closeNativeHandle(const void *data, bool metadata)
             return BAD_VALUE;
         }
     } else {
-        ALOGE("Warning: Not of type video meta buffer");
-        return BAD_VALUE;
+        ALOGW("Warning: Not of type video meta buffer");
     }
     return rc;
 }
+#endif
 
 /*===========================================================================
  * FUNCTION   : getMatchBufIndex
@@ -1335,6 +1336,7 @@ camera_memory_t *QCameraVideoMemory::getMemory(int index, bool metadata) const
     if (index >= mMetaBufCount || (!metadata && index >= mBufferCount))
         return NULL;
     if (metadata) {
+#ifdef USE_MEDIA_EXTENSIONS
         int i;
         media_metadata_buffer *packet = NULL;
         for (i = 0; i < mMetaBufCount; i++) {
@@ -1350,6 +1352,9 @@ camera_memory_t *QCameraVideoMemory::getMemory(int index, bool metadata) const
             CDBG_HIGH("No free video meta memory");
             return NULL;
         }
+#else
+        return mMetadata[index];
+#endif
     } else {
         return mCameraMemory[index];
     }
