b043ab22809e9761b1fd17b2ade30333aee6ba54
Ashwin Ramesh <ashwinr64@gmail.com>
2016-12-06 10:58:22 +0530

    onyx: Use the common GPU Blobs
    Change-Id: I194868ff061a46fd8e0e3f302d4794b80b4d2db6

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,9 @@ write_headers
 # The standard blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
 
+# Adreno Blobs
+printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+
 # We are done!
 write_footers
 
