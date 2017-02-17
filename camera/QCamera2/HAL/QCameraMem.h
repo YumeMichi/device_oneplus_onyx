61c08f3387648caaff463491d01de4aa8afbdedf
Yin-Chia Yeh <yinchiayeh@google.com>
2017-02-16 16:52:53 -0800

    QCamera2: Fix HAL1 callback cookie
    Test: HIDL HAL1 preview up and running
    Bug: 30985004
    Change-Id: I085f163de6c1c6552925c8241e744c723697d544

--- camera/QCamera2/HAL/QCameraMem.h
+++ camera/QCamera2/HAL/QCameraMem.h
@@ -163,6 +163,7 @@ private:
 class QCameraStreamMemory : public QCameraMemory {
 public:
     QCameraStreamMemory(camera_request_memory getMemory,
+                        void* cbCookie,
                         bool cached,
                         QCameraMemoryPool *pool = NULL,
                         cam_stream_type_t streamType = CAM_STREAM_TYPE_DEFAULT);
@@ -179,6 +180,7 @@ public:
 
 protected:
     camera_request_memory mGetMemory;
+    void* mCallbackCookie;
     camera_memory_t *mCameraMemory[MM_CAMERA_MAX_NUM_FRAMES];
 };
 
@@ -186,7 +188,7 @@ protected:
 // framework. They are allocated from /dev/ion or gralloc.
 class QCameraVideoMemory : public QCameraStreamMemory {
 public:
-    QCameraVideoMemory(camera_request_memory getMemory, bool cached);
+    QCameraVideoMemory(camera_request_memory getMemory, void* cbCookie, bool cached);
     virtual ~QCameraVideoMemory();
 
     virtual int allocate(int count, int size);
@@ -219,7 +221,7 @@ class QCameraGrallocMemory : public QCameraMemory {
         BUFFER_OWNED,
     };
 public:
-    QCameraGrallocMemory(camera_request_memory getMemory);
+    QCameraGrallocMemory(camera_request_memory getMemory, void* cbCookie);
     void setNativeWindow(preview_stream_ops_t *anw);
     virtual ~QCameraGrallocMemory();
 
@@ -246,6 +248,7 @@ private:
     preview_stream_ops_t *mWindow;
     int mWidth, mHeight, mFormat, mStride, mScanline;
     camera_request_memory mGetMemory;
+    void* mCallbackCookie;
     camera_memory_t *mCameraMemory[MM_CAMERA_MAX_NUM_FRAMES];
     int mMinUndequeuedBuffers;
 };
