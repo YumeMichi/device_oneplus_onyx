adec14484de5bdfeee1ba2ea55f61d68c48c33fb
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    onyx: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 	$(LOCAL_PATH)/overlay \
 	$(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
