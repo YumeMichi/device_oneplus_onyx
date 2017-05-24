c0e00726e8f896bfb238142e09a9d705aa712874
Sultanxda <sultanxda@gmail.com>
2017-05-24 15:42:02 -0700

    onyx: Use GCC 4.8 for kernel compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,8 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := onyx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
