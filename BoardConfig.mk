a618a4c5ecac67f19486fa3aed1dc6a8ae007b0f
CertifiedBlyndGuy <rjmurdok@linux.com>
2017-05-31 17:06:01 -0600

    onyx: add gesture support
    commit ed84555a770a3a7550e9d44d0e1743992937dcc7
    Author: Jake Weinstein <xboxlover360@gmail.com>
    Date:   Sun Jun 12 11:15:12 2016 -0400
    
        onyx: move gestures file to power directory
    
    commit c3bc5bc80f0e0848c15aa2765c07031be0bebce3
    Author: Jake Weinstein <xboxlover360@gmail.com>
    Date:   Mon Apr 25 00:08:42 2016 -0400
    
        onyx: enable Keyhandler
    
    commit db12563dbdad937fe5a80afded9e9018e35c58c2
    Author: Jake Weinstein <xboxlover360@gmail.com>
    Date:   Sun Apr 24 19:33:36 2016 -0400
    
        onyx: Unify swipe gestures
    
    commit c00d1075840597037896884b695809cf91e7f5c8
    Author: Willi Ye <williye97@gmail.com>
    Date:   Sat Apr 23 17:40:56 2016 +0200
    
        Add gestures
    
        Change-Id: I9eb90ec318163db5fb2ae6e004ab7bff5e9a1f76
    
    Change-Id: I7cf057fa9b96e03aeef774547dc43fcb4ec3074f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,4 +170,10 @@ ifeq ($(BOARD_USES_QCNE),true)
 TARGET_LDPRELOAD := libNimsWrap.so
 endif
 
+TARGET_POWER_GESTURE_FILE := device/oneplus/onyx/power/gestures.c
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_DRAW_V_NODE := "/proc/touchpanel/flashlight_enable"
+TARGET_DRAW_O_NODE := "/proc/touchpanel/camera_enable"
+TARGET_DRAW_ARROW_LEFT_NODE := "/proc/touchpanel/music_enable"
+
 -include vendor/oneplus/onyx/BoardConfigVendor.mk
