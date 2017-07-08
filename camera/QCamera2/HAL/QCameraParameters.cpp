037a42f0edd9fec9bf23eb8815bc6dceae7f6449
Sultanxda <sultanxda@gmail.com>
2017-07-08 12:41:15 -0700

    QCamera2: Remove sharpness cap
    Allow users to increase sharpness to what they want.
    
    Change-Id: I6676af7dd98c764ca8841579b9fd606c7d8fc18b

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -5199,9 +5199,6 @@ int32_t QCameraParameters::setSharpness(int sharpness)
     CDBG("%s: Setting sharpness %s", __func__, val);
 
     int32_t value = sharpness;
-    if (value > MAX_REAL_SHARPNESS)
-        value = MAX_REAL_SHARPNESS;
-
     return AddSetParmEntryToBatch(m_pParamBuf,
                                   CAM_INTF_PARM_SHARPNESS,
                                   sizeof(value),
