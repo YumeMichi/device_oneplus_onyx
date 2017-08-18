88841a42d8dc3526720b089bb73a70bf7816f47b
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    onyx: init: Update for O-MR1
    Change-Id: Iedd68dae1b1b2efb7df70b585168d70b9df9dc64

--- init/Android.mk
+++ init/Android.mk
@@ -21,6 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_onyx.cpp
 LOCAL_MODULE := libinit_onyx
 
