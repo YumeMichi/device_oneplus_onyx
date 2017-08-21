2e4e0a7ba0867c23179a83d1c1cb3b67b3418a14
YumeMichi <do4suki@gmail.com>
2017-08-21 10:42:15 +0800

    onyx: Add assertions on TrustZone
    * Taken from oppo common, Adapted to onyx on O
    
    Change-Id: If81493f7081cb651664f5bcd7efcb2384d3ea308
    Signed-off-by: YumeMichi <do4suki@gmail.com>

---
+++ recovery_updater/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/updater/include
+
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_oppo
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_STATIC_LIBRARY)
