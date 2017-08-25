05cb08716ee01d2e1b44568dffa0d3ca7e231377
Michael Bestas <mkbestas@lineageos.org>
2017-08-25 14:54:43 +0300

    onyx: Build libbt-vendor
    Change-Id: Ie7f49e04c38a5f34289d0414b21eddb49e06038b

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
