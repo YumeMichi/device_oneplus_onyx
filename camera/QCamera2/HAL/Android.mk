25dbd8086264ac9a8b58452cc78113c9eda70d5a
Ashwin Ramesh <ashwinr64@lineageos.org>
2017-08-26 10:56:35 +0530

    onyx: camera: Fix for O-MR1
    Change-Id: Ie712f5bd16105591ccf9f6d8e3973ecc7364636a
    
    Change-Id: Ic76cfff3a5bfe46c0ea6974e1ffce9fb7ed069c6

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -36,6 +36,8 @@ endif
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
+        frameworks/native/libs/nativewindow/include \
+        frameworks/native/libs/nativebase/include \
         $(call project-path-for,qcom-display)/libgralloc \
         $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
@@ -55,7 +57,10 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
+LOCAL_STATIC_LIBRARIES := libbase libarect
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
