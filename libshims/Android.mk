19a56342101ee98db7e051e654b300d32e81c773
YumeMichi <do4suki@gmail.com>
2017-08-26 17:29:48 +0000

    onyx: libshims: Add missing libsensor dependency
    Change-Id: I2048fa3e9d64a71e48a59217b2714f947cfd5314
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := sensors.cpp
-LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_SHARED_LIBRARIES := libgui libsensor
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE := libshims_sensors
 LOCAL_MODULE_TAGS := optional
