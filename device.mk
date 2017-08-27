5af89a0ef257e8af564c504f9a33f0f9da26d6a6
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:32:05 +0200

    onyx: Kill unused hostapd configs
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- device.mk
+++ device.mk
@@ -260,10 +260,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny
+    p2p_supplicant_overlay.conf
 
 PRODUCT_PACKAGES += \
     wcnss_service \
