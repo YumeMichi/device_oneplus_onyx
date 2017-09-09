d8232e2351dec5a55f6ae3954deedb0e53ccdfcc
YumeMichi <do4suki@gmail.com>
2017-09-09 15:13:08 +0000

    QCamera2: Fix build for O-MR1
    Change-Id: Iac1a0c0b983d9473a23841ea1696c10ce995b91f
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -35,6 +35,7 @@ endif
 
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
+        framework/native/include \
         frameworks/native/include/media/openmax \
         $(call project-path-for,qcom-display)/libgralloc \
         $(call project-path-for,qcom-media)/libstagefrighthw \
@@ -55,9 +56,9 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libnativewindow
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libhidltransport libsensor android.hidl.token@1.0-utils
+LOCAL_STATIC_LIBRARIES := libarect
 
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
