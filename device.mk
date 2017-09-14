c1233da96bbd3877f4c698c236605b1f21c54f24
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    onyx: Add seccomp policy
    Change-Id: I581a3f577bff1a5c6433a51bbdc52ac2446ac47e

--- device.mk
+++ device.mk
@@ -219,6 +219,11 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     libxml2
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
