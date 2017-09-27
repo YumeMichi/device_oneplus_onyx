96475b54596c9ef46f02c6989414748663a90060
Michael Bestas <mkbestas@lineageos.org>
2017-09-27 23:36:53 +0300

    onyx: gps: Cleanup makefiles and remove useless files
    Change-Id: I66848d2e220817ae61642d7211f7eabf5e4efcea

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -1,10 +1,1 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-
-LOCAL_PATH := $(call my-dir)
-
-GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
-
-#call the subfolders
-include $(addsuffix Android.mk, $(GPS_DIR_LIST))
-
-endif#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+include $(call all-subdir-makefiles)
