831b90d211007774080125834b2b8ca821b11f20
Bruno Martins <bgcngm@gmail.com>
2017-09-28 23:09:06 +0100

    onyx: Move shell init scripts to vendor
    Change-Id: Ib73a5ae51b00481fc8adf3f40a0816d95930387b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,7 +24,7 @@ LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 # Device init files
