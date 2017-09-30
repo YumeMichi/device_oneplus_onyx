08c47be1df822baacab20302b77ba18109103c9f
Ethan Chen <intervigil@gmail.com>
2017-09-29 23:14:23 -0700

    onyx: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- device.mk
+++ device.mk
@@ -40,9 +40,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
-PRODUCT_PACKAGES += \
-    init.qcom.bt.sh
-
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/vendor/etc/permissions/com.dsi.ant.antradio_library.xml \
