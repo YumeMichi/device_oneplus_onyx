0198c88c5f290670569ffd893c815438a53521a3
Michael Bestas <mkbestas@lineageos.org>
2017-10-15 18:01:00 +0300

    onyx: gps: Remove unused quipc.conf & xtwifi.conf
    Change-Id: I66391197c5bbf4e754d80dee492ab4b4096e811c

--- device.mk
+++ device.mk
@@ -158,9 +158,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IPv6
 PRODUCT_PACKAGES += \
