d0014e4b2ee2203a68bc7371d5b740d30a6fac1c
Bruno Martins <bgcngm@gmail.com>
2017-11-16 22:40:39 +0000

    onyx: Include DRM service
    Change-Id: I13757ca7e611d4a90477c319f4fe124ce632755e

--- device.mk
+++ device.mk
@@ -120,6 +120,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OneplusDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
