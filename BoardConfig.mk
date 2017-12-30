e64fd747d4c974d395dfcc7ae46999883cb8162f
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    onyx: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,10 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 
 
 # Power
+TARGET_HAS_LEGACY_POWER_STATS := true
+TARGET_HAS_NO_WIFI_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
