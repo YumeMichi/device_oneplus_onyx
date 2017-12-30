b0025702206e5344c3b160663f576c26f1032d78
Paul Keith <javelinanddart@gmail.com>
2017-12-30 23:56:33 +0100

    onyx: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: If012d242f5bcc4905a11336e7b46ff6b427ca9e8

--- device.mk
+++ device.mk
@@ -218,8 +218,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8974
+    android.hardware.power@1.0-service-qti
 
 # RIL
 PRODUCT_PACKAGES += \
