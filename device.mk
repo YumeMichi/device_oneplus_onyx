97d87bfa27a7eb66acbaa2afd506e30069e26280
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 18:59:09 +0200

    onyx: Move sec_config to vendor
    Change-Id: I84df8b8e5b1b2b6bb09b9f43b8d711961c47a30b

--- device.mk
+++ device.mk
@@ -168,7 +168,7 @@ PRODUCT_PACKAGES += \
 
 # IRSC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
