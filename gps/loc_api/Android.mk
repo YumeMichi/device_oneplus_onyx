faf35208ec2918dee49d688e8f53fd012f0ce399
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-17 20:02:36 -0700

    onyx: gps: Delete libloc_api-rpc
    * This isn't needed
    
    Change-Id: I7b4b33256fac5ae71b2e556ed04f0069d1aa9f39

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -2,13 +2,6 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 
 LOCAL_PATH := $(call my-dir)
 
-# add RPC dirs if RPC is available
-ifneq ($(TARGET_NO_RPC),true)
-
-GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api-rpc-50001/
-
-endif #TARGET_NO_RPC
-
 GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
 
 #call the subfolders
