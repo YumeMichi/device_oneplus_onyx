ce9445a5af8f367ae34053dd5c9ea1e9403b616a
Michael Bestas <mkbestas@lineageos.org>
2018-01-25 23:13:40 +0200

    onyx: Drop audiod
    * It's now deprecated
    
    Change-Id: I00c8bdc09da4ba39508314ec8feba67d60e4fb04

--- device.mk
+++ device.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    audiod \
     audio.a2dp.default \
     audio_policy.msm8974 \
     audio.primary.msm8974 \
