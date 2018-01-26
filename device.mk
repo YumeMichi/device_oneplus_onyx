8c9d9b9ca241ce635ed28aba01ac70f76b23f7af
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    onyx: Add more binderized HIDL HALs
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
     * Following considerations regarding those without any change:
    
       - Legacy camera HAL1 support is not finished yet so there's
         no way to test at the moment, therefore left untouched.
       - Sensors and NFC left out of the game because wouldn't work
         otherwise.
       - Keep passthrough mode for GNSS.
    
    Change-Id: Id50291488d655187aa013c51bdd6890dca010564

--- device.mk
+++ device.mk
@@ -89,7 +89,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     audiod \
     audio.a2dp.default \
     audio_policy.msm8974 \
@@ -104,6 +106,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
@@ -136,6 +139,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
@@ -170,7 +174,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Keystore
 PRODUCT_PACKAGES += \
