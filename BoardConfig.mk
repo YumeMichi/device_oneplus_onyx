a8e9983c49c75bffd3ea008b4126aface086c7fe
Michael Bestas <mkbestas@lineageos.org>
2018-02-27 20:00:45 +0000

    onyx: Get rid of recovery-only fstab
     * Mounting /system partition is handled in kernel now, however
       removing the entry from fstab caused issues building the OTA.
       The workaround was to have a separate fstab, but turns out
       that simply setting the recoveryonly flag does the trick because
       those are then ignored during a normal Android boot.
    
    Change-Id: I2944384d0a1c41bc9f9f51e2e29daff2bed0a0f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,7 +155,7 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.full
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 TARGET_RIL_VARIANT := caf
