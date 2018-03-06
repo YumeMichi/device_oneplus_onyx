bc13e4e63a681f29c63ac1eb316639469f3ea221
Planet-X <planetx.dev@gmail.com>
2018-03-06 16:19:11 +0100

    onyx: Add offline charging LED controller
     * Taken from zenfone3, modified for onyx.
    
    Add a simple script that watches the battery capacity when charging offline.
    Every 15 seconds the battery level is evaluated and the battery light gets
    updated with the corresponding color.
    
    Change-Id: I773646f0d94456c68f8e6d5334a7c50a265cd6d4
    Signed-off-by: Planet-X <planetx.dev@gmail.com>
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,6 +27,16 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+# Device config scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.chargerled.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.chargerled.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Device init files
 
 include $(CLEAR_VARS)
