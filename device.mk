28abbe74c998d6e0479f2759bda0651ac19dc5cd
Bruno Martins <bgcngm@gmail.com>
2018-06-26 18:26:55 +0100

    onyx: Drop deprecated OpenGLRenderer props
     * In Android 8.1 and later, only the ro.zygote.disable_gl_preload
       property still applies. All other properties have been removed.
       Reference: https://source.android.com/devices/graphics/renderer
    
    Change-Id: Ib43c22ea3285e5fe397da8a7a5cac1c3a27a2f36

--- device.mk
+++ device.mk
@@ -16,7 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
