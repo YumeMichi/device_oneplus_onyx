1608ceae683b289bcb56aac55b165eaff4c41547
Michael Bestas <mkbestas@lineageos.org>
2018-09-18 07:00:52 +0000

    onyx: Switch to 1.1 power HAL
    Change-Id: I6b1f0bcdea83c6124ddaa6042640c78157ca6f79

--- device.mk
+++ device.mk
@@ -243,7 +243,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Recovery
 PRODUCT_PACKAGES += \
