d34b82a47c544a87fb8bfa109f6ec65d1e40d1f5
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-08 13:25:19 +0100

    onyx: doze: Set entry timestamp before registering sensor
    Change-Id: Ifa8aa1ded029c3c7be44ac94355e6814c080be44

--- doze/src/org/lineageos/settings/doze/TiltSensor.java
+++ doze/src/org/lineageos/settings/doze/TiltSensor.java
@@ -79,9 +79,9 @@ public class TiltSensor implements SensorEventListener {
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
         submit(() -> {
+            mEntryTimestamp = SystemClock.elapsedRealtime();
             mSensorManager.registerListener(this, mSensor,
                     SensorManager.SENSOR_DELAY_NORMAL, BATCH_LATENCY_IN_MS * 1000);
-            mEntryTimestamp = SystemClock.elapsedRealtime();
         });
     }
 
