f955f5baec03b134110561628e2f9147af64b7ab
Joey <joey@lineageos.org>
2019-01-09 18:02:45 +0000

    onyx: Build Trust HAL
    Change-Id: If45f063d72ad675f60983ba482b4bfeab875da3c
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
