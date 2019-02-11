af6a42884266c0d80989e37cd2be091a4b2ffa27
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 15:35:46 +0200

    onyx: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I3968611e0e150b73df9f2c00974bdb150726c2cd

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 PRODUCT_PACKAGES += \
