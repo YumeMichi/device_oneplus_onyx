7b82fd3c4cd574fd6700536960d422c721c07469
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    onyx: Update GraphicBuffer shim for Q
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
    
    Could not load /vendor/lib/hw/camera.vendor.msm8974.so from default namespace: dlopen failed: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/system/vendor/lib/libFNVfbEngineLib.so"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

--- libshims/camera.cpp
+++ libshims/camera.cpp
@@ -26,3 +26,9 @@ extern "C" void _ZN7android13GraphicBufferC1Ejjij(
   _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
       inWidth, inHeight, inFormat, inUsage, requestorName);
 }
+
+extern "C" void _ZN7android13GraphicBuffer4lockEjPPvPiS3_(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+
+extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(uint32_t inUsage, void** vaddr) {
+    _ZN7android13GraphicBuffer4lockEjPPvPiS3_(inUsage, vaddr, nullptr, nullptr);
+}
