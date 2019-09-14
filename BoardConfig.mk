d1f605e11393c5a56277977415dd1139645236f5
Bruno Martins <bgcngm@gmail.com>
2019-09-14 22:00:15 +0100

    onyx: Remove obsolete RIL variant board flag
    There's no CAF variant anymore.
    
    Change-Id: Ief74f8b2aa861b0d16ffab9b52304006008d2444

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,9 +158,6 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 
-# RIL
-TARGET_RIL_VARIANT := caf
-
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
