9d9ae1c669822b91c61fb37430fce32d8401eab8
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    onyx: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- device.mk
+++ device.mk
@@ -243,7 +243,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Recovery
 PRODUCT_PACKAGES += \
