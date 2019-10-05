afe42f2b897e9f56daf065903e09dc44c0510347
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    onyx: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,8 +164,6 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 
 
 # Power
-TARGET_HAS_LEGACY_POWER_STATS := true
-TARGET_HAS_NO_WLAN_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
