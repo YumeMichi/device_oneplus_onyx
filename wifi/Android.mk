037205431e96f11e12771b793091abac64ad1b61
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-12-13 12:28:22 +0530

    onyx: Move hostapd configs to vendor
     * Required after https://github.com/LineageOS/android_system_qcom/commit/2443024a1aa680a6dd36d6d51006656bd5046d4a
    
    Change-Id: I265a1d03216a5c7bda726af1e2f8accd4c3fb1fd

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -24,7 +24,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd_default.conf
 include $(BUILD_PREBUILT)
 
@@ -32,7 +32,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd.accept
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.accept
 include $(BUILD_PREBUILT)
 
@@ -40,7 +40,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd.deny
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
