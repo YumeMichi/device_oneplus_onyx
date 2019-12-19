19d55b65297ae85594ea4ddaec0d849d1d6266c6
Stephane Lee <stayfan@google.com>
2019-12-19 14:57:04 -0800

    onyx: Uprev health HAL to 2.1
    Bug: b/137790244
    Test: lshal debug (health service)
    
    Change-Id: Id15d203bd791d0867972da98f6998610723b9332

--- device.mk
+++ device.mk
@@ -194,8 +194,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Lights
 PRODUCT_PACKAGES += \
