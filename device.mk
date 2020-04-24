6af226f70f9bae803a680baf0647a844a20e193f
YumeMichi <do4suki@gmail.com>
2020-04-24 12:04:45 +0800

    Revert "onyx: Switch to 1.1 power HAL"
    This reverts commit 077848dd137c4e1ca1239942485757d17bc98b55.
    
    Reason for revert: Perf rework.
    
    Change-Id: I32dea3197588318be74c1a4bd2a388e47a2e2699
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # Recovery
 PRODUCT_PACKAGES += \
