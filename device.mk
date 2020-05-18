52fe6ba5acd81ff15949958bf4f7b2937b432b4e
Arne Coucheron <arco68@gmail.com>
2020-05-18 06:29:20 +0200

    onyx: Build legacy GNSS HAL
    Change-Id: I56095b9c3bba065779c90b6aeb61ebfed6af7e77

--- device.mk
+++ device.mk
@@ -163,8 +163,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service \
+    android.hardware.gnss@1.0-impl.legacy \
+    android.hardware.gnss@1.0-service.legacy \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
