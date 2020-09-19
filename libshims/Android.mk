70c19e8aa9490699fc977e368c4e3a279422a095
Han Wang <416810799@qq.com>
2020-09-19 09:52:14 +0200

    onyx: Make shim for AudioSystem::setErrorCallback()
     * Commit 000c3e4 in fw/av changed setErrorCallback() to
       addErrorCallback() to support multiple instances, but
       this breaks our old blob.
    
    Change-Id: I03495e5746a4ad75a09a0e24bb7faf265a8b0bda

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -27,3 +27,13 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := libaudioclient_shim.cpp
+LOCAL_MODULE := libaudioclient_shim
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SHARED_LIBRARIES := libaudioclient
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
