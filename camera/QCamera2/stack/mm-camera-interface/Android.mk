b4b153acc18edd30c93cffea269ae46956289cdf
Bruno Martins <bgcngm@gmail.com>
2020-09-22 14:47:33 +0100

    onyx: camera: Drop usage of LOCAL_COPY_HEADERS
    Google deprecated LOCAL_COPY_HEADERS in Android 11,
    so use LOCAL_EXPORT_C_INCLUDE_DIRS instead.
    
    Ref: https://android.googlesource.com/platform/build/soong/+/master/docs/best_practices.md#headers.
    
    Change-Id: Id68bc83cf74f2af0037331ebe4d17819021b88e8

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -22,10 +22,6 @@ endif
 
 LOCAL_CFLAGS += -D_ANDROID_
 
-LOCAL_COPY_HEADERS_TO := mm-camera-interface
-LOCAL_COPY_HEADERS += ../common/cam_intf.h
-LOCAL_COPY_HEADERS += ../common/cam_types.h
-
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
@@ -40,6 +36,7 @@ LOCAL_CFLAGS += -Wall -Werror
 
 LOCAL_MODULE           := libmmcamera_interface
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog
+LOCAL_HEADER_LIBRARIES += camera_common_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
