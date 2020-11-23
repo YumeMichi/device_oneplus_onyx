153ea0a48ca5c6f40f9d3cfc9eef3fc11a00b6ce
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-23 14:46:29 -0500

    onyx: Cleanup dead targets
    Offending entries:
    audio_policy.msm8974
    copybit.msm8974
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- device.mk
+++ device.mk
@@ -103,7 +103,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
-    audio_policy.msm8974 \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
@@ -160,7 +159,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm8974 \
     gralloc.msm8974 \
     hwcomposer.msm8974 \
     memtrack.msm8974 \
